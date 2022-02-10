b7e6450e3cb47e68e9c70bf862e40cdeb086d4b5
Haidar <haidaraltaf@gmail.com>
2022-02-10 15:53:26 +0000

    budgetapp

---
+++ budgetapp.py
@@ -0,0 +1,99 @@
+# class budget:
+#     def __init__money(self):
+#         balence = [food, clothes, entertainment, leftover]
+#         self.food = food
+#         return balence
+    
+# food = budget 
+# input("amount: ")
+# clothes = input("amount: ")
+# entertainment = input("amount: ")
+# leftover = input("amount: ")
+
+
+
+# print(food, clothes, entertainment, leftover)
+
+# food.deposit(20)
+# food.transfer()
+# clothes.withdraw(50)
+# leftover.withdraw("treat yo\' self")
+
+
+class Budget: # Creates template/blueprint that can be used by each object/category
+
+    def __init__(self, name): # Pass name of category with the object
+
+        self.name = name
+        self.money = list()
+
+
+    def deposit(self, amount): # Might need to pass other variables
+        print(amount)
+        print("Deposited")
+        self.money.append(amount)
+
+        # Write Code for deposit function
+
+
+
+
+    def withdraw(self, amount): # Might need to pass other variables
+        print(amount)
+        print("Withdrawn")
+        self.money.append(-amount)
+
+
+        # Write Code for withdraw function
+
+
+
+    def transfer(self, amount, account): # Might need to pass other variables
+        print(amount)
+        print("Transfered")
+        if self.get_balance() > amount:
+            self.withdraw(amount)
+            account.deposit(amount)
+            
+
+            
+        # Write Code for withdraw function
+
+
+
+    def get_balance(self): # Might need to pass other variables
+        bal_total = 0
+        print("Balance")
+        for x in self.money:
+            bal_total += x
+        return bal_total
+        # Write Code for withdraw function
+
+
+
+
+
+print("\nfood")
+food = Budget("Food") # Creating an instance/object of the Budget class and passing in a name
+food.deposit(4545)
+food.withdraw(785)
+print(food.get_balance())
+
+
+
+
+print("\nclothes")
+clothes = Budget("Clothes")
+clothes.deposit(245)
+clothes.withdraw(63)
+clothes.transfer(7,food)
+print(clothes.get_balance())
+
+
+
+print("\nentertainment")
+entertainment = Budget("Entertainment")
+entertainment.deposit(3634)
+entertainment.withdraw(5365)
+entertainment.transfer(76, clothes)
+print(entertainment.get_balance())
