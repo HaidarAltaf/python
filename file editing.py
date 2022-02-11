7c4cc8176f4c75b6e6efc1034d9558af2de142f6
Haidar <haidaraltaf@gmail.com>
2022-02-11 11:02:41 +0000

    file tampering

--- file editing.py
+++ file editing.py
@@ -1,4 +1,17 @@
-file = open("composers.txt")
-lines = file.readlines
-print(lines)
+# file = open("composers", "r+")
+# lines = file.read()
 
+# for line in range(1, 9):
+#     if line ==3 :
+#         print("Ludwig Van Beethoven")
+#     else:
+#         print("REDACTED")
+# file.close
+
+
+
+file2 = open("composers", "w")
+file2.write
+for n in range(1,10):
+    newline = ("widely recognised artists\n")
+    file2.write(newline)
